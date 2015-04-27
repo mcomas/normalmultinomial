f68b23cb427a5243b79f187149d55693c841f27a
marc_comas <marc.comas@gmail.com>
2015-04-27 15:21:47 +0200

    C++11 set as default

---
+++ script.R/testing_speed.R
@@ -0,0 +1,20 @@
+Rcpp::sourceCpp('src/fitting.cpp')
+library(microbenchmark)
+
+mu = c(1,1,1,1)
+Sigma = diag(c(1,1,1,1))
+
+X = rnormalmultinomial(mu, Sigma, size = rep(100, 1000))
+
+microbenchmark(
+  normalmultinomial_fitting(X, niter = 2, nsim = 100),
+  normalmultinomial_fitting(X, niter = 2, nsim = 100, version = 2),
+  normalmultinomial_fitting(X, niter = 2, nsim = 100, version = 3, nthreads = 1),
+  normalmultinomial_fitting(X, niter = 2, nsim = 100, version = 3, nthreads = 2),
+  normalmultinomial_fitting(X, niter = 2, nsim = 100, version = 3, nthreads = 3),
+  normalmultinomial_fitting(X, niter = 2, nsim = 100, version = 3, nthreads = 4),
+  times = 10)
+
+
+
+library(mixpack)
