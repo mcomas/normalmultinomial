3363a8d060e98dbc4fc3afd5ec6f2bcabd94b2f9
mcomas <marc.comas@gmail.com>
2016-07-13 12:38:23 +0200

    More descriptives

--- R/random.R
+++ R/random.R
@@ -5,6 +5,8 @@
 #' @param mu mean parameter for the mean in a normal distribution
 #' @param sigma parameter for the sigma in a normal distribution
 #' @return Sample matrix
+#' @examples
+#' rnormal(10, mu = c(0,0), sigma = matrix(c(2,1,1,2), nrow=2))
 #' @export
 rnormal <- function(n, mu, sigma) {
   if( ! (is.vector(mu) & is.numeric(mu)) ){
@@ -23,7 +25,7 @@ rnormal <- function(n, mu, sigma) {
 #'
 #' @param n matrix with the probabilities used to generate the sample
 #' @param size number of trials en each observation
-#' @param prob
+#' @param prob probability of observations (vector) or specific to each observation (matrix)
 #' @return multinomial sample in a matrix
 #' @examples
 #' rmultinomial(10, c(10, 100), c(0.2,0.8))
@@ -62,12 +64,38 @@ rmultinomial <- function(n, size, prob) {
 #'
 #' @param n number of observations
 #' @param size number of trials
-#' @param mu mean parameter for the mean in a aln-normal distribution
-#' @param sigma parameter for the sigma in a aln-normal distribution
+#' @param mu mean parameter for the mean in the aln-normal distribution
+#' @param sigma parameter for the sigma in the aln-normal distribution
 #' @return Sample matrix
+#' @examples
+#' rnormalmultinomial(10, 5, mu = c(0,0), sigma = matrix(c(2,1,1,2), nrow=2))
 #' @export
-rnormalmultinomial <- function(mu, sigma, size, probs = FALSE) {
-  res = .Call('normalmultinomial_rnormalmultinomial', PACKAGE = 'normalmultinomial', mu, sigma, size, sample(.Random.seed, 1))
+rnormalmultinomial <- function(n, size, mu, sigma, probs = FALSE) {
+  if(! (is.vector(n) & is.numeric(n)) ){
+    error("'n' must be numeric")
+  }
+  if(! (is.numeric(prob) & (is.vector(prob) | is.matrix(prob) )) ){
+    error("'prob' must be numeric vector or a numeric matrix")
+  }
+  if( ! (is.vector(size) & is.numeric(size)) ){
+    error("'size' must be a numeric vector")
+  }
+  if(length(n) == 1){
+    N = n
+  }else{
+    N = length(n)
+  }
+  if( ! (is.vector(mu) & is.numeric(mu)) ){
+    error("'mu' must be a numeric vector")
+  }
+  if( ! (is.matrix(sigma) & is.numeric(sigma)) ){
+    error("'sigma' must be a numeric matrix")
+  }
+  if( det(sigma) <= 0 ){
+    error("'sigma' must be a positive-definite matrix")
+  }
+  SIZE = rep_len(size, N)
+  res = .Call('normalmultinomial_rnormalmultinomial', PACKAGE = 'normalmultinomial', mu, sigma, SIZE, sample(.Random.seed, 1))
   if(probs){
     names(res) = c('probs', 'counts')
     res[['probs']] = res[['probs']] / apply(res[['probs']], 1, sum)
--- test/test.R
+++ test/test.R
@@ -1,10 +1,10 @@
 library(mixpack)
 
-(N <- 1000)
+(N <- 10000)
 (MU <- c(0,0))
 
 SIGMA.ilr <- matrix(c(1,0,
-                       0,1), nrow = 2) * 0.05
+                       0,1), nrow = 2)
 ilrB = t(clr_coordinates(t(matrix(unlist(ilr_basis(3)), ncol=2))))
 ilr_to_alr = MASS::ginv(ilrB) %*% matrix(c(1,0,-1,
                                                      0,1,-1), ncol=2)
@@ -17,11 +17,16 @@ set.seed(1)
 sample_multinomial = rmultinomial(n = N, size = c(10, 100), prob = c(0.2,0.8))
 
 set.seed(1)
-sample_nm = rnormalmultinomial(size= rep(5, N), mu = MU, sigma = SIGMA, probs = TRUE)
+sample_nm = rnormalmultinomial(N, 100, mu = MU, sigma = SIGMA, probs = TRUE)
 
 library(ggtern)
 probs = as.data.frame(sample_nm$probs)
+counts = as.data.frame(sample_nm$counts)
 ggtern() +
-  geom_point(data=probs, aes(x = V1, y = V2, z = V3))
+  geom_point(data=probs, aes(x = V1, y = V2, z = V3), col='red', alpha=0.05) +
+  theme_bw()
+ggtern() +
+  geom_point(data=counts, aes(x = V1, y = V2, z = V3), col='red', alpha=0.015, size = 3) +
+  theme_bw()
 
 # plot(as.data.frame(ilr_coordinates(probs)), asp = 1)
