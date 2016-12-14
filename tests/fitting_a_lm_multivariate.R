5590ccaec91fef22daf7eef70745d52b3fb482cb
mcomas <marc.comas@gmail.com>
2016-12-14 19:50:16 +0100

    More regression

--- tests/fitting_a_lm_multivariate.R
+++ tests/fitting_a_lm_multivariate.R
@@ -18,33 +18,48 @@ generate_mv_normal_rnd = function(n, dim){
 }
 
 Y = as.matrix(parliament2015[,c('jxsi', 'psc', 'pp', 'catsp', 'cs', 'cup')])
-X = cbind(1,boot::logit(parliament2015$birth.cat/parliament2015$pop))
-
+X = cbind(1,
+          boot::logit(parliament2015$birth.cat/parliament2015$pop),
+          rnorm(nrow(parliament2015)) )
+# parliament2015$ss.agri,
+# parliament2015$lat,
+# parliament2015$long
 init = initialize_with_dm(Y)
 E = init$H
-B = matrix(c(init$MU,
-             rep(0, length(init$MU))), nrow = length(init$MU))
+B = t(apply(E, 2, function(y){
+  b = solve(t(X) %*% X) %*% (t(X) %*% y)
+}))
 
 MU = X %*% t(B)
-SIGMA = init$SIGMA
+SIGMA = diag(mean(diag(init$SIGMA)), ncol(E))
 
 Z = generate_mv_normal_rnd(1000, nrow(SIGMA))
 
 pB = B
 err = 1
-eps = 0.0001
+eps = 0.001
 max.em.iter = 100
 iter = 0
 while(err > eps & iter < max.em.iter){
-  fit = lapply(1:nrow(Y), function(i) expectedMonteCarloFirstMoment(Y[i,], MU[i,], SIGMA, Z, E[i,]))
-  y = t(sapply(fit, function(f) f[[2]]))
-  m = lm(y~0+X)
-  B = cbind(t(coef(m)))
-  err = max(B-pB)
-  pB = B
-  MU = cbind(m$fitted.values)
-  SIGMA = cov(m$residuals)
+
+  FIT = lapply(1:nrow(Y), function(i) expectedMonteCarloFast(Y[i,], MU[i,], SIGMA, Z, E[i,]))
+  E = t(matrix(sapply(FIT, function(fit) fit[[2]]), nrow=ncol(MU)))
+
+  B = t(apply(E, 2, function(y){
+    b = solve(t(X) %*% X) %*% (t(X) %*% y)
+  }))
+  MU = X %*% t(B)
+
+  L = lapply(FIT, function(fit) fit[[3]] - fit[[2]] %*% t(fit[[2]]) )
+  SIGMA = diag(mean(diag(Reduce(`+`, L) / nrow(E))), ncol(E))
+
   iter = iter + 1
+  print(err <- max(pB-B))
+  pB = B
 }
 
-summary(m)
+
+summary(m1<-lm(E~0+X))
+
+
+exp(clr_coordinates(inv_ilr_coordinates(t(B))))
--- tests/logistic_vs_normalbinomial.R
+++ tests/logistic_vs_normalbinomial.R
@@ -10,9 +10,26 @@ fit = nm_fit(Y, eps = 0.00000001, verbose = TRUE)
 
 plot(1:nrow(Y), Y[,1]/(Y[,1]+Y[,2]))
 points(1:nrow(Y), mod$fitted.values)
-points(1:nrow(Y), fit$eYpected[,1], col='red')
+points(1:nrow(Y), fit$expected[,1], col='red')
 points(1:nrow(Y), rep(inv_ilr_coordinates(fit$mu)[1], nrow(Y)), col='red')
 
 y = c(ilr_coordinates(fit$eYpected))
 summary(m1<-glm(Y~Age, family = binomial(logit), data=menarche))
 summary(m2<-lm(y~Age, data=menarche))
+
+
+###
+# Parliament
+data("parliament2015")
+X = parliament2015[, c('cup', 'cs', "birth.cat", "pop")]
+X$birth.cat = X$birth.cat / X$pop
+X = X[order(X$birth.cat),]
+
+mod = glm(cbind(cup,cs)~birth.cat, family = binomial(logit), data=X)
+fit = nm_fit(as.matrix(X[,c('cup','cs')]), eps = 0.00001, verbose = TRUE)
+
+plot(1:nrow(X), X[,1]/(X[,1]+X[,2]))
+points(1:nrow(X), mod$fitted.values)
+points(1:nrow(X), fit$expected[,1], col='red')
+points(1:nrow(X), rep(inv_ilr_coordinates(fit$mu)[1], nrow(X)), col='red')
+
