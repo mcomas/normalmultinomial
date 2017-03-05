6c36c41cadb09a7f5d6e9156e51d9dbaa359dc62
Marc <marc.comas@gmail.com>
2017-03-05 10:07:09 +0100

    Mod | K instead of Mod | k

--- src/stepE.cpp
+++ src/stepE.cpp
@@ -30,7 +30,7 @@ arma::mat rotation(arma::colvec x, arma::colvec y){
   arma::mat T2 = - u * u.t();
   arma::mat T3 = - v * v.t();
   arma::mat T4 = arma::join_rows(u,v) * arma::join_cols(arma::join_rows(cost,-sint),arma::join_rows(sint,cost)) * arma::join_rows(u,v).t();
-  //Rcout << T1 << T2 << T3 << T4 << std::endl;
+
   arma::mat res = T1 + T2 + T3 + T4;
 
   return(res);
@@ -63,7 +63,6 @@ Rcpp::List expectedMonteCarlo(arma::vec x, arma::vec mu_ilr, arma::mat sigma_ilr
   arma::mat SAMPLING_MU = arma::repmat(sampling_mu, nsim, 1);
 
   arma::mat mu12 = (sampling_mu * inv_sigma * sampling_mu.t() - mu.t() * inv_sigma * mu)/2;
-  //double mult_const = mvf_multinom_const(x);
   arma::mat D = inv_sigma * (mu-sampling_mu.t());
 
   arma::vec M0 = arma::vec(nsim);
@@ -87,76 +86,19 @@ Rcpp::List expectedMonteCarlo(arma::vec x, arma::vec mu_ilr, arma::mat sigma_ilr
         M2.tube(i,j) += C % Ap.col(j) / K;
       }
     }
-    // Rotation
+    // Rotating through simplex vertices
     if(K == 2){
       Z = -Z;
     }else{
-      arma::mat R = rotation(Simplex.col( (v+1) % k), Simplex.col(v));
+      arma::mat R = rotation(Simplex.col( (v+1) % K), Simplex.col(v));
       Z = Z * R;
     }
-
   }
   for(int s=0; s<nsim;s++){
     M1.row(s) = M1.row(s) * ALR_TO_ILR_trans;
     M2.slice(s) = ALR_TO_ILR * M2.slice(s) * ALR_TO_ILR_trans;
   }
 
-  // arma::mat Z1 = Z;
-  // arma::mat Z2 = -Z1;
-  //
-  //
-  // arma::mat Ap1 = SAMPLING_MU + Z1 * sampling_sigma_chol;
-  // arma::mat Ap2 = SAMPLING_MU + Z2 * sampling_sigma_chol;
-  //
-  //
-  //
-  // //arma::vec loglik1 = mu12(0,0) + mult_const + mvf_multinom_mult(Ap1, x) + Ap1 * D;
-  // //arma::vec loglik2 = mu12(0,0) + mult_const + mvf_multinom_mult(Ap2, x) + Ap2 * D;
-  // arma::vec loglik1 = mu12(0,0) + mvf_multinom_mult(Ap1, x) + Ap1 * D;
-  // arma::vec loglik2 = mu12(0,0) + mvf_multinom_mult(Ap2, x) + Ap2 * D;
-  //
-  // //Rcpp::Rcout << loglik1 << std::endl;
-  // //Rcpp::Rcout << loglik2 << std::endl;
-  //
-  // double cmax12 = std::max(max(loglik1), max(loglik2));
-  //
-  // arma::vec lik1 = exp(loglik1 - cmax12);
-  // arma::vec lik2 = exp(loglik2 - cmax12);
-  //
-  // //Rcpp::Rcout << lik1 << std::endl;
-  // //Rcpp::Rcout << lik2 << std::endl;
-  //
-  // //lik1.replace(arma::datum::nan, 0);
-  // //lik2.replace(arma::datum::nan, 0);
-  //
-  //
-  // // lik_st initialized to ones in case lik1 is too small
-  // arma::vec lik1_st = lik1 / mean(lik1);
-  // arma::vec lik2_st = lik2 / mean(lik2);
-  //
-  // //lik1_st.replace(arma::datum::nan, 1);
-  // //lik2_st.replace(arma::datum::nan, 1);
-  //
-  // //Rcpp::Rcout << lik1_st << std::endl;
-  //
-  // //arma::vec M0 = arma::vec(nsim);
-  // //arma::mat M1 = arma::mat(nsim, k);
-  // //arma::cube M2 = arma::cube(k,k, nsim);
-  //
-  // M0 =  (lik1 + lik2) / 2;
-  // for(int i = 0;i < k; i++){
-  //   arma::mat C1 = Ap1.col(i) % lik1_st;
-  //   arma::mat C2 = Ap2.col(i) % lik2_st;
-  //   M1.col(i) = (C1 + C2) / 2;
-  //   for(int j = 0;j < k; j++){
-  //     M2.tube(i,j) = (C1 % Ap1.col(j) + C2 % Ap2.col(j)) / 2;
-  //   }
-  // }
-  //
-  // for(int s=0; s<nsim;s++){
-  //   M1.row(s) = M1.row(s) * ALR_TO_ILR_trans;
-  //   M2.slice(s) = ALR_TO_ILR * M2.slice(s) * ALR_TO_ILR_trans;
-  // }
   return Rcpp::List::create(M0, M1, M2);
 }
 
